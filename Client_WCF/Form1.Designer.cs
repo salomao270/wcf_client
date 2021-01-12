5ec7a38788f8f30f2999b617eca0a43e07190a4d
salomao270 <salomao270@hotmail.com>
2021-01-12 00:17:27 -0300

    release project files

---
+++ Client_WCF/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Client_WCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnValidateEmail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter an email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(156, 94);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(191, 22);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // btnValidateEmail
+            // 
+            this.btnValidateEmail.Location = new System.Drawing.Point(156, 135);
+            this.btnValidateEmail.Name = "btnValidateEmail";
+            this.btnValidateEmail.Size = new System.Drawing.Size(78, 30);
+            this.btnValidateEmail.TabIndex = 2;
+            this.btnValidateEmail.Text = "Validate Email";
+            this.btnValidateEmail.UseVisualStyleBackColor = true;
+            this.btnValidateEmail.Click += new System.EventHandler(this.btnValidateEmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 303);
+            this.Controls.Add(this.btnValidateEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnValidateEmail;
+    }
+}
+
---
+++ Client_WCF/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Client_WCF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidateEmail_Click(object sender, EventArgs e)
+        {
+            //creating an instance of my WCF service
+            var wcf_service = new Wcf_ExampleReference.EmailServiceClient();
+
+            //using the method from my WCF service to validate an email
+            if (wcf_service.ValidateEmail(txtEmail.Text))
+                MessageBox.Show("Remote service response: it is a valid email format");
+            else
+                MessageBox.Show("Remote service response: it is an invalid email format");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
